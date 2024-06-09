22c941e996e66de9bc468adfcd44d8c0790f2389
pilisuriano <pilu_suriano@hotmail.com>
2024-06-08 22:33:42 -0300

    Mas archivos

---
+++ frontend/src/data/Products.js
@@ -0,0 +1,48 @@
+const products = [
+    {
+        _id: "1",
+        name: "Nike Slim Shirt",
+        image: "/images/p1.jpg",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: "2",
+        name: "Adidas Fit Shirt",
+        image: "/images/p2.jpg",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        price: 100,
+        countInStock: 20,
+        rating: 4.0,
+        numReviews: 5,
+    },
+    {
+        _id: "3",
+        name: "Lacoste Free Shirt",
+        image: "/images/p3.jpg",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        price: 220,
+        countInStock: 1,
+        rating: 4.8,
+        numReviews: 17,
+    },
+    {
+        _id: "4",
+        name: "Nike Slim Pant",
+        image: "/images/p4.jpg",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        price: 78,
+        countInStock: 5,
+        rating: 4.5,
+        numReviews: 14,
+    },
+];
+
+export default products;
